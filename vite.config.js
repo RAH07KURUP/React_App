80dfd8ec107bf1ec3714cb92bfacef3a06dc5eec
Rahul Kurup <rpsg1819@gmail.com>
2024-05-19 14:07:21 +0530

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "tailwindcss"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+    // other configurations...
+  server: {
+    host: '0.0.0.0',
+    port: 5173, // or your desired port
+  },
+  },
+})
